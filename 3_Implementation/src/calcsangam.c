bff516310b38c2adbb3275b5d2fadbfaeb015910
99003786 <78857588+99003786@users.noreply.github.com>
2021-02-14 15:22:29 +0530

    Added More Operation And All TrigonoMetric Func

--- 3_Implementation/src/calcsangam.c
+++ 3_Implementation/src/calcsangam.c
@@ -4,8 +4,8 @@ int main()
 {
     float san1,san2,res,mults,divs,subs,adds,sine,cosi,tani,coseci,seci,coti,arcsini,arccosi,arctani,arccoseci,arcseci,arccoti,angsani,ang;
     int symbols;
-printf("For Addition press-1\nFor Substraction press-2\nFor Multiplication press-3\nFor Divison press-4\nTo Find Sin press-5\n");    
-    printf("Enter Symbol(1,2,3,4,5,6,7,8,9,10,11,12)\n");
+printf("For Addition press-1\nFor Substraction press-2\nFor Multiplication press-3\nFor Divison press-4\nAll Trigonometric Answer Will Be In Radian\nFor Sin press-5\nFor Cos press-6\nFor Tan press-7\nFor Cosec press-8\nFor Sec press-9\nFor Cot press-10\nFor Sin Inverse press-11\nFor Cos Inverse press-12\nFor Tan Inverse press-13\nFor Cosec Inverse press-14\nFor Sec Inverse press-15\nFor Cot Inverse press-16\n");    
+    //printf("Enter Symbol(1,2,3,4,5,6,7,8,9,10,11,12)\n");
     scanf("%d",&symbols);
    switch(symbols)
     {
@@ -48,20 +48,76 @@ printf("For Addition press-1\nFor Substraction press-2\nFor Multiplication press
           printf("Enter The Value Of Angle\n");
           scanf("%f",&ang);
           sine=sin(ang);
-          printf("sin=%f",sine);
+          printf("sin=%f\n",sine);
         break;
          case 6:
-printf("Enter The Value Of angle\n");
-scanf("%f",&ang);
-cosi=cos(ang);
-printf("cos=%f",cosi);
+            printf("Enter The Value Of angle\n");
+           scanf("%f",&ang);
+           cosi=cos(ang);
+           printf("cos=%f\n",cosi);
           break;
                case 7:
           printf("Enter The Value Of Angle\n");
           scanf("%f",&ang);
           tani=tan(ang);
-          printf("tan=%f",tani);
+          printf("tan=%f\n",tani);
           break;
+       case 8:
+          printf("Enter The Value Of Angle\n");
+            scanf("%f",&ang);
+           coseci=1/sin(ang);
+            printf("cosec=%f\n",coseci);
+            break;
+        case 9:
+        printf("Enter The Value Of Angle\n");
+         scanf("%f",&ang);
+         seci=1/cos(ang);
+       printf("sec=%f\n",seci);
+        break;
+       case 10:
+        printf("Enter The Value Of Angle\n");
+       scanf("%f",&ang);
+        coti=1/tan(ang);
+          printf("cot=%f\n",coti);
+        break;
+       case 11:
+         printf("Enter The Value\n");
+            scanf("%f",&ang);
+           arcsini=asin(ang);
+           printf("sin inverse=%f\n",arcsini);
+          break;
+      case 12:
+        printf("Enter The Value\n");
+        scanf("%f",&ang);
+        arccosi=acos(ang);
+        printf("cos inverse=%f\n",arccosi);
+        break;
+     case 13:
+     printf("Enter The Value\n");
+      scanf("%f",&ang);
+     arctani=atan(ang);
+     printf("tan inverse=%f\n",arctani);
+     break;
+    case 14:
+     printf("Enter The Value\n");
+     scanf("%f",&ang);
+    arccoseci=1/asin(ang);
+   printf("cosec inverse=%f\n",arccoseci);
+   break;
+   case 15:
+   printf("Enter The Value\n");
+   scanf("%f",&ang);
+   arcseci=1/acos(ang);
+   printf("sec inverse=%f\n",arcseci);
+    break;
+   case 16:
+   printf("Enter The Value\n");
+   scanf("%f",&ang);
+   arccoti=1/atan(ang);
+    printf("cot inverse=%f\n",arccoti);
+   break;
+
+
 
     }
  return 0;
